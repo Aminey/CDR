9ec4ca597f25340d6705279491c9b24675e64a8d
donovanfung <navonod@ucla.edu>
2016-05-09 04:48:03 -0700

    build_airfoil updates
    Added inertia calculations.
    There still seems to be something wrong with the code because im getting
    unreasonable values for x-centroid. Will fix it soon.

--- build_airfoil.m
+++ build_airfoil.m
@@ -3,6 +3,7 @@
 % NACA 2415
 close all; clc; clear all;
 
+%% 2415 Airfoil 
 m = 0.02;               % 
 p = 0.4;                % max camber
 t = 0.15;               % max thickness 
@@ -11,6 +12,18 @@ nx = 500;               % number of increments
 dx = c/nx;  
 x = 0:dx:c;             % even spacing
 
+%% Structural elements (arbitrary inputs for now)
+skin.t = 0.01;                               % thickness of skin
+spar.t = 0.05;                               % thickness of spar
+str.A_upp = 0.05;                            % area of each upper stringer
+str.A_low = 0.05;                            % area of each lower stringer
+caps.A = 0.1;                                % area of spar caps
+spar.x = x(120);                                                      % choose x location of spar
+str.x_upp = [x(1), x(50), x(150), x(200), x(250),x(300), x(350)];     % choose x locations of upper stringers
+str.x_low = [x(1), x(50), x(150), x(200), x(250),x(300), x(350)];     % choose x locations of lower stringers
+
+
+%% Create Airfoil from equation
 yc = zeros(1,nx+1);
 yt = zeros(1,nx+1);
 yU0 = zeros(1,nx+1);
@@ -41,8 +54,6 @@ yU = yU0(1:i_xend);
 yL = yL0(1:i_xend);
 
 %% Spars
-spar.x = [x(20), x(30)];                     % choose x locations
-spar.t = 0.5;                                % thickness of spar
 spar.x = [spar.x,x(end)];                    % x location of spar
 spar.n = length(spar.x);                     % number of spars
 spar.i = round(spar.x./dx)+1;                % index of spars  
@@ -52,26 +63,16 @@ spar.A = spar.t*spar.h;                      % area of spar
 
 
 %% Stringers
-str.x_upp = [x(5), x(10), x(15), x(20), x(25),x(30)];     % x locations of upper stringers
 str.n_upp = length(str.x_upp);                            % number of upper stringers
 str.i_upp = round(str.x_upp./dx)+1;                       % indices of upper stringers
-str.A_upp = 0.5;                                          % area of each upper stringer
-
-str.x_low = [x(5), x(10), x(15), x(20), x(25),x(30)];     % x locations of lower stringers
 str.n_low = length(str.x_low);                            % number of lower stringers
 str.i_low = round(str.x_low./dx)+1;                       % indices of lower stringers
-str.A_low = 0.5;                                          % area of each lower stringer
 
-
-%% Nodes and Skins
-% Nodes include spar caps and stringers
+%% Nodes (based on TA's definition: node = skin and spar cap)
 node.x_upp = [str.x_upp,spar.x];            % x location of upper stringers and spars
 node.x_upp = sort(node.x_upp);              % sort ascending
-
 % skin should be broken into smaller elements for higher accuracy of calculation
 % break one skin element into two by adding one more node in between
-
-skin.t = 5;                                  % thickness of skin
 skin.x_upp = zeros(1,2*length(node.x_upp)-1);
 for i = 1:length(node.x_upp)-1
     skin.x_upp(2*i-1) = node.x_upp(i);
@@ -93,7 +94,7 @@ for i = 1:skin.n_upp
     skin.Cy_upp(i) = (yU(skin.i_upp(i+1)) + yU(skin.i_upp(i)))/2;
 end
 
-% lower part
+% lower nodes
 node.x_low = [str.x_low,spar.x];
 node.x_low = sort(node.x_low);
 
@@ -109,7 +110,7 @@ skin.n_low = length(skin.x_low)-1;
 skin.l_low = zeros(1,skin.n_low);
 skin.A_low = zeros(1,skin.n_low);
 skin.Cx_low = zeros(1,skin.n_low);
-skinL.Cy_low = zeros(1,skin.n_low);
+skin.Cy_low = zeros(1,skin.n_low);
 
 for i = 1:skin.n_low
     skin.l_low(i) = sqrt((yL(skin.i_low(i+1)) - yL(skin.i_low(i)))^2 + (skin.x_low(i+1) - skin.x_low(i))^2);
@@ -118,24 +119,19 @@ for i = 1:skin.n_low
     skin.Cy_low(i) = (yL(skin.i_low(i+1)) + yL(skin.i_low(i)))/2;
 end
 
-%% centroid of the wing section
+%% Spar Caps
+caps.x = spar.x;
+caps.n = 2*length(caps.x);
+caps.y_upp = yU(spar.i);
+caps.y_low = yL(spar.i);
+
+% Centroids of caps are at the x and y locations: caps.Cy = caps.C,
+
+%% Centroid of the overall wing section
 % initial value
-Cx_sum = sum(str.x_upp) + sum(str.x_low) + sum(spar.x) + sum(skin.Cx_low) + sum(skin.Cx_upp) ;
-Cy_sum = sum(yU(str.i_upp)) + sum(yL(str.i_low)) + sum(spar.Cy) + sum(skin.Cy_low) + sum(skin.Cy_upp);
-A_sum = str.A_upp*str.n_upp + str.A_low*str.n_low + sum(spar.A) + sum(skin.A_low) + sum(skin.A_upp);
-
-% spars
-% for i = 1:n
-%     Cx_sum = Cx_sum + x(i)*area(i);
-%     Cy_sum = Cy_sum + Cy(i)*area(i);
-%     A_sum = A_sum + area(i);
-% end
-
-%
-%
-% please finish the part for stringers, spar caps and skins
-%
-%
+Cx_sum = 2*(sum(caps.x)) + sum(str.x_upp) + sum(str.x_low) + sum(spar.x) + sum(skin.Cx_low) + sum(skin.Cx_upp) + sum(caps.x);
+Cy_sum = sum(caps.y_low) + sum(caps.y_upp) + sum(yU(str.i_upp)) + sum(yL(str.i_low)) + sum(spar.Cy) + sum(skin.Cy_low) + sum(skin.Cy_upp);
+A_sum = caps.A*caps.n + str.A_upp*str.n_upp + str.A_low*str.n_low + sum(spar.A) + sum(skin.A_low) + sum(skin.A_upp);
 
 Cx = Cx_sum/A_sum;
 Cy = Cy_sum/A_sum;
@@ -146,32 +142,77 @@ ylim([-0.3 0.3])
 hold on
 xlim([0,c]);
 plot(str.x_upp,yU(str.i_upp),'or',str.x_low,yL(str.i_low),'or','markersize',5);
-plot([x(1),x(1)],[yU(i(1)),yL(i(1))],'b',[x(end),x(end)],[yU(end),yL(end)],'b','Linewidth',3);
-% plot(x,yU(i),'sg',x,yL(i),'sg','markersize',7);
+% plot([x(1),x(1)],[yU(i(1)),yL(i(1))],'b',[x(end),x(end)],[yU(end),yL(end)],'b','Linewidth',3);
+rectangle('Position',[x(spar.i(1))-spar.t/2, yL(spar.i(1)), spar.t, yU(spar.i(1))-yL(spar.i(1))]);
+rectangle('Position',[x(spar.i(2))-spar.t/2, yL(spar.i(2)), spar.t, yU(spar.i(2))-yL(spar.i(2))]);
+plot(spar.x,yU(spar.i),'sg',spar.x,yL(spar.i),'sg','markersize',7);
 % plot([x(end),x(end)],[yU(end),yL(end)]);
 scatter(Cx,Cy,'m*')
 ylabel('y (m)')
 xlabel('x (m)')
 grid on
 
+%% Make CCW coordinate system for shear flow
+x_CCW = zeros(1,length(x));
+yU_CCW = zeros(1,length(x));
+yL_CCW = zeros(1,length(x));
+
+for i = 1:length(x)
+    x_CCW(i) = x(end - i + 1);
+    yU_CCW(i) = yU(end - i + 1);
+    yL_CCW(i) = yL(end - i + 1);
+end
+y_CCW = [yU_CCW yL_CCW(2:end)];         % the 2:end gets rid of the extra initial 0 value
+x_CCW = [x_CCW x(2:end)];
+
 %% Area moments of inertia
-% initial value
-Ixx = 0;
-Iyy = 0;
-Ixy = 0;
 
-% spars
-% for i = 1:n
-%     Ixx = Ixx + spar.t*height(i)^3/12 + area(i)*(Cy(i)-Cy)^2;
-%     Iyy = Iyy + spar.t^3*height(i)/12 + area(i)*(x(i)-Cx)^2;
-%     Ixy = Ixy + area(i)*(Cy(i)-Cy)*(x(i)-Cx);
-% end
+for i = 1:length(spar.x)
+    spar.Ixx(i) = (spar.t*spar.h(i)^3)/12 + spar.A(i)*(spar.Cy(i) - Cy)^2;
+    spar.Iyy(i) = (spar.h(i)*spar.t^3)/12 + spar.A(i)*(spar.x(i) - Cx)^2;
+    spar.Ixy(i) = spar.A(i)*(spar.Cy(i) - Cy)*(spar.x(i) - Cx);
+end
 
-%
-%
-% please add contrbution of spar caps, stringers and skin panels. You can use the previous code as a reference.
-%
-%
+for i = 1:skin.n_upp
+    skin.Ixx_upp(i) = skin.A_upp(i)*(skin.Cy_upp(i) - Cy)^2;
+    skin.Iyy_upp(i) = skin.A_upp(i)*(skin.Cx_upp(i) - Cx)^2;
+    skin.Ixy_upp(i) = skin.A_upp(i)*(skin.Cy_upp(i) - Cy)*(skin.Cx_upp(i) - Cx);
+end
+
+for i = 1:skin.n_low
+    skin.Ixx_low(i) = skin.A_low(i)*(skin.Cy_low(i) - Cy)^2;
+    skin.Iyy_low(i) = skin.A_low(i)*(skin.Cx_low(i) - Cx)^2;
+    skin.Ixy_low(i) = skin.A_low(i)*(skin.Cy_low(i) - Cy)*(skin.Cx_low(i) - Cx);
+end
+
+for i = 1:str.n_upp
+    str.Ixx_upp(i) = str.A_upp*(yU(str.i_upp(i)) - Cy)^2;
+    str.Iyy_upp(i) = str.A_upp*(str.x_upp(i) - Cx)^2;
+    str.Ixy_upp(i) = str.A_upp*(yU(str.i_upp(i)) - Cy)*(str.x_upp(i) - Cx);
+end
+
+for i = 1:str.n_low
+    str.Ixx_low(i) = str.A_low*(yL(str.i_low(i)) - Cy)^2;
+    str.Iyy_low(i) = str.A_low*(str.x_low(i) - Cx)^2;
+    str.Ixy_low(i) = str.A_low*(yL(str.i_low(i)) - Cy)*(str.x_low(i) - Cx);
+end
+
+for i = 1:length(caps.x)
+    caps.Ixx_upp(i) = caps.A*(caps.y_upp(i) - Cy)^2;
+    caps.Iyy_upp(i) = caps.A*(caps.x(i) - Cx)^2;
+    caps.Ixy_upp(i) = caps.A*(caps.y_upp(i) - Cy)*(caps.x(i) - Cx);
+end
+
+for i = 1:length(caps.x)
+    caps.Ixx_low(i) = caps.A*(caps.y_low(i) - Cy)^2;
+    caps.Iyy_low(i) = caps.A*(caps.x(i) - Cx)^2;
+    caps.Ixy_low(i) = caps.A*(caps.y_low(i) - Cy)*(caps.x(i) - Cx);
+end
+
+Ixx = sum(spar.Ixx)+sum(skin.Ixx_upp)+sum(skin.Ixx_low)+sum(str.Ixx_upp)+sum(str.Ixx_low)+sum(caps.Ixx_low);
+Iyy = sum(spar.Iyy)+sum(skin.Iyy_upp)+sum(skin.Iyy_low)+sum(str.Iyy_upp)+sum(str.Iyy_low)+sum(caps.Iyy_low);
+Ixy = sum(spar.Ixy)+sum(skin.Ixy_upp)+sum(skin.Ixy_low)+sum(str.Ixy_upp)+sum(str.Ixy_low)+sum(caps.Ixy_low);
+
 
 %% coordiante transformation: new origin at centroid
 %
