4a7a68afe33d9b029c94113cc5dfde4820bd95d6
jtkam <jaredtkam@gmail.com>
2016-05-09 01:39:11 -0700

    changes

--- Shear_Flow_0.m
+++ Shear_Flow_0.m
@@ -73,9 +73,39 @@ y = zeros(1,length(airfoil.booms));
 i_A1 = find(ismember(x,spars.x(1)));        % i don't think this is right.... need to fiddle with it.
 A1 = sum(delta_A(i_A1(1):i_A1(2)));
 A2 = A_total - A1;
+%%
+syms q01.sea q02.sea
+syms q01.alt q02.alt
+%% EQ 1
+for k = 1:5
+    for j = 1:length(z)
+        M_0.sea + Sy.sea(j,k)*(x_quarterchord) - Sx.sea(j,k)*(0) = 2*A1*q01.sea(j,k) + 2*A2*q02.sea(j,k) + Term_2.sea;
+        M_0.alt + Sy.alt(j,k)*(x_quarterchord) - Sx.alt(j,k)*(0) = 2*A1*q01.alt(j,k) + 2*A2*q02.alt(j,k) + Term_2.alt;
+    end
+end
+%% EQ 2
+% dtheta_dz_1
+delta_term_1(i) = zeros(1,length(x)); %% this is wrong. doesnt go from 1 to length(x). goes over front section
+delta_term_2.sea(i) = zeros(1,length(x));
+delta_term_2.alt(i) = zeros(1,length(x));
+delta_term_3.sea(i) = zeros(1,length(x));
+delta_term_3.alt(i) = zeros(1,length(x));
 
-syms q01 q02
+for i = 1:length(x)-1 % "Sum CCW over front section"
+    delta_term_1(i) = (((x(i+1)-x(i))^2 + (y(i+1) - y(i))^2)^0.5)/skin.thickness;
+    delta_term_2.sea(i) = Booms.sea * x(i) * (((x(i+1)-x(i))^2 + (y(i+1) - y(i))^2)^0.5)/thickness(i);
+    delta_term_2.alt(i) = Booms.alt * x(i) * (((x(i+1)-x(i))^2 + (y(i+1) - y(i))^2)^0.5)/thickness_1;
+    delta_term_3.sea(i) = Booms.sea * y(i) * (((x(i+1)-x(i))^2 + (y(i+1) - y(i))^2)^0.5)/thickness(i);
+    delta_term_3.alt(i) = Booms.alt * y(i) * (((x(i+1)-x(i))^2 + (y(i+1) - y(i))^2)^0.5)/thickness_1;
 
-M_0.sea + Sy.sea*(x_quarterchord) - Sx.sea*(0) = 2*A1*q01.sea + 2*A2*q02.sea + Term_2.sea;
-M_0.alt + Sy.alt*(x_quarterchord) - Sx.alt*(0) = 2*A1*q01.alt + 2*A2*q02.alt + Term_2.alt;
+end
+sum_term_1 = sum(delta_term_1(:));
+sum_term_2.sea = sum(delta_term_2.sea(:));
+sum_term_2.alt = sum(delta_term_2.alt(:));
+sum_term_3.sea = sum(delta_term_3.sea(:));
+sum_term_3.alt = sum(delta_term_3.alt(:));
 
+
+
+dtheta_dz.sea = (1/(2*A1*G)) * (q01.sea*(sum_term_1) + (q01.sea-q02.sea)*(y_bot - y_top)/spar.thickness + ((Sy.sea*Ixy - Sx.sea*Ixx)/(Ixx*Iyy-Ixy^2))*(sum_term_2.sea) + ((Sx.sea*Ixy-Sy.sea*Iyy)/(Ixx*Iyy-Ixy^2))*(sum_term_3.sea));
+dtheta_dz.alt = (1/(2*A1*G)) * (q01.sea*(sum_term_1) + (q01.alt-q02.alt)*(y_bot - y_top)/spar.thickness + ((Sy.alt*Ixy - Sx.alt*Ixx)/(Ixx*Iyy-Ixy^2))*(sum_term_2.alt) + ((Sx.alt*Ixy-Sy.alt*Iyy)/(Ixx*Iyy-Ixy^2))*(sum_term_3.alt));
