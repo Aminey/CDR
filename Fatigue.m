5ad90002269c24e439914be1861441590edf0bb7
jtkam <jaredtkam@gmail.com>
2016-05-31 19:45:40 -0700

    changes

--- Fatigue.m
+++ Fatigue.m
@@ -19,24 +19,29 @@ cycles.alt = zeros(1,length(x));
 
 % loop will find critical crack length "a" at each point along the airfoil
 for i = 1:length(x)
-    a.sea(i) = (K_IC/(f*sigma_z.sea(i,1,1)))^2/pi;
-    a.alt(i) = (K_IC/(f*sigma_z.alt(i,1,1)))^2/pi;
+    a.sea(i) = (K_IC/(f*sigma_z.sea(i,1,1)))^2/pi / 1000;   %mm
+    a.alt(i) = (K_IC/(f*sigma_z.alt(i,1,1)))^2/pi / 1000;   %mm
 end
 
 a.sea_frac(:) = a.sea(:) * frac;
 a.alt_frac(:) = a.alt(:) * frac;
 
 %% Cycles to Failure
+%   note that sigma must be in N/mm^2, hence the division by 1000000
+for i = 1:(length(x)+1)/2
+    cycles.sea(i) = (1/(c*(sigma_z.sea(i,1,4)/1000000*pi^0.5)^m)) * (((a.sea(i)^(1-m/2))/(1-m/2))-((a.sea_frac(i)^(1-m/2))/(1-m/2)));
+    cycles.alt(i) = (1/(c*(sigma_z.alt(i,1,4)/1000000*pi^0.5)^m)) * (((a.alt(i)^(1-m/2))/(1-m/2))-((a.alt_frac(i)^(1-m/2))/(1-m/2)));
 
-for i = 1:length(x)
-    cycles.sea(i) = (1/(c*(abs(sigma_z.sea(i,1,1)-sigma_z.sea(i,1,4))*pi^0.5)^m)) * (((a.sea(i)^(1-m/2))/(1-m/2))-((a.sea_frac(i)^(1-m/2))/(1-m/2)));
-    cycles.alt(i) = (1/(c*(abs(sigma_z.alt(i,1,1)-sigma_z.alt(i,1,4))*pi^0.5)^m)) * (((a.alt(i)^(1-m/2))/(1-m/2))-((a.alt_frac(i)^(1-m/2))/(1-m/2)));
+end
+for i = (length(x)+3)/2:length(x)
+    cycles.sea(i) = (1/(c*(sigma_z.sea(i,1,1)/1000000*pi^0.5)^m)) * (((a.sea(i)^(1-m/2))/(1-m/2))-((a.sea_frac(i)^(1-m/2))/(1-m/2)));
+    cycles.alt(i) = (1/(c*(sigma_z.alt(i,1,1)/1000000*pi^0.5)^m)) * (((a.alt(i)^(1-m/2))/(1-m/2))-((a.alt_frac(i)^(1-m/2))/(1-m/2)));
 end
 
-if max(cycles.sea) >= max(cycles.alt)
-    cycles.fail = max(cycles.sea);
+if min(cycles.sea) <= min(cycles.alt)
+    cycles.fail = min(cycles.sea);
 else
-    cycles.fail = max(cycles.alt);
+    cycles.fail = min(cycles.alt);
 end
 
 disp('Cycles to Failure =')
